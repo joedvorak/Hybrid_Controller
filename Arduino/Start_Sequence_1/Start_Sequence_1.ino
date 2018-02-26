f5ec9889499682f4f1951a0dc46ead806f1ecb88
samday95 <34352281+samday95@users.noreply.github.com>
2018-02-26 11:37:31 -0500

    Update Start_Sequence_1.ino

--- Arduino/Start_Sequence_1/Start_Sequence_1.ino
+++ Arduino/Start_Sequence_1/Start_Sequence_1.ino
@@ -1,7 +1,6 @@
 //must include these each time
 #include <stdlib.h>
 #include <SPI.h>
-
 #include "mcp_can.h"
 #include "can_ext.h"
 
@@ -28,98 +27,46 @@ pinMode(startSwitchPin, OUTPUT);
 
 void startEngine()
 {
-    digitalWrite(keySwitchPin, HIGH);
-    delay(500);
-}
-
-
-void startSwitch()
-{
     byte nPriority;
     byte nSrcAddr;
     byte nDestAddr;
     byte nData[8];
+    
     int nDataLen;
     long lPGN;
-  
     char sString[80];
-  
-  // Check for received J1939 messages
-    if(j1939Receive(&lPGN, &nPriority, &nSrcAddr, &nDestAddr, nData, &nDataLen) == 0)
-    {
-       if((int)lPGN == 0xF004)
-       {
-          sprintf(sString, "%0b ", nData[4]);
-          Serial.print(sString);
-          Serial.print("\n\r");
-          sprintf(sString, "%0b ", nData[3]);
-          Serial.print(sString);
-          Serial.print("\n\r");
-          int engineRPM = ((nData[4] << 8) + nData[3]) >> 3;
-          sprintf(sString, "%d", engineRPM);
-          Serial.print(sString);
-          Serial.print("\n\r");
-      }
-     int startSwitchTimer = millis();
+    
+    digitalWrite(keySwitchPin, HIGH);
+    delay(500);
+    digitalWrite(startSwitchPin,HIGH);
+    int startSwitchTimer = millis();                    //start timer for starter switch
+    
     boolean done = false;
     while(done != true)
-    {
-       digitalWrite(startSwitchPin,HIGH);
-       if(engineRPM >= 600)
-       {
-          digitalWrite(startSwitchPin, LOW);
-          done = true;
-       }
-       else if(millis() >= startSwitchTimer+10000);
-       {
-          digitalWrite(startSwitchPin, LOW);
-          done = true;
-       }
-    }
-}
-
-void loop()
-{
-    byte nPriority;
-    byte nSrcAddr;
-    byte nDestAddr;
-    byte nData[8];
-    int nDataLen;
-    long lPGN;
-  
-    char sString[80];
-  
-    digitalWrite(keySwitchPin, HIGH);
-    delay(500);
-
-    int startSwitchTimer = millis();
-  
-    if(j1939Receive(&lPGN, &nPriority, &nSrcAddr, &nDestAddr, nData, &nDataLen) == 0)
-    {
-        if((int)lPGN == 0xF004)
+    {   
+        //check j1939 for RPM
+        if(j1939Receive(&lPGN, &nPriority, &nSrcAddr, &nDestAddr, nData, &nDataLen) == 0)
         {
-            sprintf(sString, "%0b ", nData[4]);
-            Serial.print(sString);
-            Serial.print("\n\r");
-            sprintf(sString, "%0b ", nData[3]);
-            Serial.print(sString);
-            Serial.print("\n\r");
-            int engineRPM = ((nData[4] << 8) + nData[3]) >> 3;
-            sprintf(sString, "%d", engineRPM);
-            Serial.print(sString);
-            Serial.print("\n\r");
+            if((int)lPGN == 0xF004)
+            {
+                sprintf(sString, "%0b ", nData[4]);
+                Serial.print(sString);
+                Serial.print("\n\r");
+                sprintf(sString, "%0b ", nData[3]);
+                Serial.print(sString);
+                Serial.print("\n\r");
+                int engineRPM = ((nData[4] << 8) + nData[3]) >> 3;
+                sprintf(sString, "%d", engineRPM);
+                Serial.print(sString);
+                Serial.print("\n\r");
+            }
         }
-    }
-    boolean done = false;
-    while(done != true)
-    {
-        digitalWrite(startSwitchPin,HIGH);
         if(engineRPM >= 600)
         {
             digitalWrite(startSwitchPin, LOW);
             done = true;
         }
-        else if(millis() >= startSwitchTimer+10000);
+        else if(millis() >= startSwitchTimer+9500);
         {
             digitalWrite(startSwitchPin, LOW);
             done = true;
